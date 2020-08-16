dc4554268eea8dd27495ad5c346ed8d37665fc87
Martin <marb61a@gmail.com>
2020-08-16 19:00:40 +0100

    added initial visual test and jes config file

---
+++ tests-snapshots/__tests__/visual.test.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('Visual Regression Testing', () => {
+    let browser;
+    let page;
+
+    // Will be ran before any tests
+    beforeAll(async function(){
+        browser = await puppeteer.launch({
+            headless: true,
+            slowMo: 0,
+            devtools: false,
+            // defaultViewport: null,
+            // args: ['--start-maximized']
+        });
+
+        page = await browser.newPage();
+    });
+
+    afterAll(async function() {
+        await browser.close();
+    });
+
+    test('', async function(){
+
+    })
+});
