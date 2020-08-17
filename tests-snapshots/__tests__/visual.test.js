e572b112b39abeeaf11a9f05d9794be011434a73
Martin <marb61a@gmail.com>
2020-08-17 19:49:38 +0100

    added mobile device snapshot tests to visual test file

--- tests-snapshots/__tests__/visual.test.js
+++ tests-snapshots/__tests__/visual.test.js
@@ -45,4 +45,27 @@ describe('Visual Regression Testing', () => {
             failureThreshold: 0.01
         });
     });
+
+    test('Mobile Snapshot', async function(){
+        await page.goto('http://www.example.com');
+        await page.waitForSelector('h1');
+        await page.emulate(puppeteer.devices['iPhone X']);
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot({
+            failureThresholdType: 'percent',
+            failureThreshold: 0.02
+        });
+    });
+
+    test('Tablet Snapshot', async function(){
+        await page.goto('http://www.example.com');
+        await page.waitForSelector('h1');
+        await page.emulate(puppeteer.devices['iPad landscape']);
+        const image = await page.screenshot();
+        expect(image).toMatchImageSnapshot({
+            failureThresholdType: 'percent',
+            failureThreshold: 0.01
+        });
+    });
+
 });
